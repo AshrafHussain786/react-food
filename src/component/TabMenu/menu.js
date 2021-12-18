aebda6505cd1a47f669859e0e9e74613baae3e22
MuhammadAshraf <ashrafhussain786@yahoo.com>
2021-12-18 16:20:16 +0500

    first commit

---
+++ src/component/TabMenu/menu.js
@@ -0,0 +1,79 @@
+const Menu = [
+     {
+        id: 1,
+        image: "images/maggi.jpg",
+        name:"maggi",
+        category: "breakfast",
+        price: "Rs. 100",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+  
+    {
+        id: 2,
+        image: "images/allupakoida.jpg",
+        name:"allu pakora",
+        category: "evening",
+        price: "Rs. 150",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+    {
+        id: 3,
+        image: "images/puri.jpg",
+        name:"kachori",
+        category: "breakfast",
+        price: "Rs. 100",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+     {
+        id: 4,
+        image: "../images/chola.jpg",
+        name:"chola",
+        category: "lunch",
+        price: "Rs. 75",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+      {
+        id: 5,
+        image: "../images/pizza.jpg",
+        name:"pizza",
+        category: "evening",
+        price: "Rs. 400",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+       {
+        id: 6,
+        image: "../images/nonvegthali.jpg",
+        name:"Nihari",
+        category: "dinner",
+        price: "Rs. 250",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+        {
+        id: 7,
+        image: "../images/sweet.jpg",
+        name:"Sweets",
+        category: "dinner",
+        price: "Rs. 200",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+        {
+        id: 8,
+        image: "../images/rajmarice.jpg",
+        name:"Rice",
+        category: "lunch",
+        price: "Rs. 130",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+        {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name:"samosa",
+        category: "evening",
+        price: "Rs. 30",
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+    },
+];
+
+
+
+export default Menu;
