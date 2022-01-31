9046f65e9b0735a6eabf830bf955a1b80468ac29
MuhammadAshraf <ashrafhussain786@yahoo.com>
2022-01-31 13:03:18 +0500

    updated files again

--- src/component/TabMenu/GalleryReact.js
+++ src/component/TabMenu/GalleryReact.js
@@ -16,7 +16,7 @@ const GalleryReact = () => {
 
     return (
         <>
-            <h1 className="mt-5 text-center main-heading">Order Your Favourite Dish</h1>
+            <h1 className="mt-3 text-center main-heading">Order Your Favourite Dish</h1>
             <hr />
             
             <div className="menu-tabs container">
@@ -30,10 +30,10 @@ const GalleryReact = () => {
             </div>
 
             {/* my main items section  */}
-            <div className="menu-items container-fluid mt-5">
+            <div className="menu-items container-fluid mt-2">
                 <div className="row">
                     <div className="col-11 mx-auto">
-                        <div className="row my-5">
+                        <div className="row my-3 mx-auto">
                             
                             {
                                 items.map((elem) => {
@@ -41,23 +41,23 @@ const GalleryReact = () => {
 
                                     return (
                                     
-                                        <div className="container" key={id}>
-                                            <div className="row">
+                                        <div className="card item col-12 col-md-6 col-lg-6 col-xl-4 my-3" key={id}>
+                                            <div className="row item-inside">
                                                 {/* for images */}
-                                                <div className="img-div">
+                                                <div className="col-12 col-md-12 col-lg-4 img img-div">
                                                     <img src={image} alt={name} className="img-fluid"/>
                                                 </div>
 
                                                 {/* menu items description */}
-                                                <div className="img-description">
-                                                    <div className="main-title">
+                                                <div className="col-12 col-md-12 col-lg-8">
+                                                    <div className="main-title pt-4 pb-3">
                                                         <h1>{name}</h1>
                                                         <p>{description }</p>
                                                     </div>                                                    
                                                     <div className="menu-price-book">
                                                         <div className="price-book-divide d-flex justify-content-between ">
                                                             <h2>Price: {price}</h2>
-                                                            <a href="ashrafhussain786@yahoo.com">
+                                                            <a href="#">
                                                                 <button className="btn btn-primary">Order Now</button>
                                                             </a>
                                                         </div>
